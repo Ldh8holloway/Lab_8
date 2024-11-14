029bfa40b657badd5caf8b793baa904ef707a40b
Ldh8holloway <ldh8@njit.edu>
2024-11-14 16:15:59 -0500

    1

---
+++ lab8.js
@@ -0,0 +1,143 @@
+//Q1
+let name = "Lucas";
+let age = 18
+console.log("name:", name);
+console.log("age:",age);
+
+//Q2
+let stringVar = "this is life"
+let numberVar = 34;
+let booleanVar =true;
+let undefinded;
+console.log("string", stringVar);
+console.log("Number:", numberVar);
+console.log("Boolean:", booleanVar);
+console.log("Undefined:", undefinedVar);
+
+//Q3
+let Rollnumebr = 15;
+let (rollnumber > 10);{
+    console.log("The RollNumber is greater than 10");
+
+}
+
+//Q4
+let isMember =true
+if (isMember){
+    console.log("Member Discount Applied");
+}
+
+//Q5
+let startvalue = 50;
+if (startvalue > 0) {
+    console.log("The start value is positve");
+}
+else if(startvalue < 0) {
+    console.log("The sart value is negative");
+}
+else{
+    console.log("The start value is zero");
+}
+
+
+//Q6
+let a =10
+let b =15
+console.log("a == b:", a == b);
+console.log("a === b:", a === b);
+console.log("a != b:", a != b);
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+
+// Q7
+let firstName = "Lucas";
+let lastName = "Holloway";
+let myAge = 18;
+console.log("Hello, " + firstName + " " + lastName + ", you are " + myAge + " years old.");
+
+// Q8
+let num = 50;
+if (num > 0 && num < 100) {
+    console.log("The number is within range.");
+}
+
+// Q9
+let hasCar = true;
+let hasLicense = true;
+let hasInsurance = true;
+if (hasCar && hasLicense && hasInsurance) {
+    console.log("You can drive legally.");
+} else {
+    console.log("Check your driving eligibility.");
+}
+
+// Q10
+let number = 21;
+if (number % 2 === 0) {
+    console.log("The number is even.");
+} else {
+    console.log("The number is odd.");
+}
+
+// Q11
+let score = 85;
+if (score >= 90) {
+    console.log("Grade A");
+} else if (score >= 80 && score <= 89) {
+    console.log("Grade B");
+} else {
+    console.log("Grade C");
+}
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <script src="myscripts.js"></script>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
