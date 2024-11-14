fc30b570f5f23e2ed01e7ec97785c493e5cf7283
Ldh8holloway <ldh8@njit.edu>
2024-11-14 16:19:16 -0500

    final

--- lab8.js
+++ lab8.js
@@ -93,51 +93,13 @@ if (score >= 90) {
 
 
 
-<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
     <title>Document</title>
-</head>
-<body>
-    <script src="myscripts.js"></script>
-</body>
+    
+    <script src="script.js"></script> 
+  </head>
+  <body>
+  </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
